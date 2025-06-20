92a0d90b0fde211f6adbdaac9e056d5f2642dc9e
Claderoki <49477117+claderoki@users.noreply.github.com>
2025-06-20 16:47:40 +0200

    no message

---
+++ api.py
@@ -0,0 +1,112 @@
+import datetime
+import json
+from typing import Dict, List
+
+import paho.mqtt.client as mqtt
+
+class History:
+    def __init__(self, payload, date: datetime.datetime):
+        self.payload = payload
+        self.date = date
+
+
+class ZigbeeObject:
+    def __init__(self, context, name):
+        self._context = context
+        self.name = name
+        self.history: List[History] = []
+
+class Switch(ZigbeeObject):
+    def on_press_release(self, payload): pass
+    def on_press(self, payload): pass
+    def up_press(self, payload): pass
+    def up_press_release(self, payload): pass
+    def down_press(self, payload): pass
+    def down_press_release(self, payload): pass
+    def off_press(self, payload): pass
+    def off_press_release(self, payload): pass
+
+
+class Bulb(ZigbeeObject):
+    BRIGHTNESS_MAX = 255
+    BRIGHTNESS_MIN = 0
+
+    @property
+    def last_known_state(self):
+        if len(self.history) == 0:
+            return {}
+        return self.history[-1].payload
+
+    def decrease_brightness(self, by: int):
+        brightness = self._get_current_brightness()
+        self.set_brightness(brightness - by)
+
+    def increase_brightness(self, by: int):
+        brightness = self._get_current_brightness()
+        self.set_brightness(brightness + by)
+
+    def set_brightness(self, brightness: int):
+        brightness = max(self.BRIGHTNESS_MIN, min(self.BRIGHTNESS_MAX, brightness))
+        self._context.set_state(self.name, {'brightness': brightness})
+
+    def _set_state(self, value: bool):
+        self._context.set_state(self.name, {"state": 'ON' if value else 'OFF'})
+
+    def set_on(self):
+        self._set_state(True)
+
+    def is_on(self):
+        last_state = self.last_known_state.get('state')
+        if last_state is None:
+            return False
+        return last_state == 'ON'
+
+    def _get_current_brightness(self):
+        return self.last_known_state.get('brightness', 125)
+
+    def is_off(self):
+        return not self.is_on()
+
+    def set_off(self):
+        self._set_state(False)
+
+
+class Context:
+    def __init__(self, host):
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.host = host
+        self._cached_devices: Dict[str, ZigbeeObject] = {}
+        self._last_known_states: dict = {}
+
+    def on_connect(self, *_):
+        for device in self._cached_devices.values():
+            self.client.subscribe(device.name)
+
+    def add_device(self, device: ZigbeeObject) -> ZigbeeObject:
+        self._cached_devices[device.name] = device
+        return device
+
+    def on_message(self, _, __, message):
+        payload = json.loads(message.payload.decode())
+
+        device = self._cached_devices.get(message.topic)
+        if device is None:
+            return
+        device.history.append(History(payload, datetime.datetime.now(tz=datetime.timezone.utc)))
+        action = payload.get('action')
+        if isinstance(device, Switch) and action is not None:
+            if hasattr(device, action):
+                getattr(device, action)(payload)
+
+        print("topic: " + message.topic + ", payload: " + str(message.payload))
+
+    def start(self):
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(self.host, 1883, 60)
+        self.client.loop_forever()
+
+
+    def set_state(self, target, state):
+        self.client.publish(f'{target}/set', json.dumps(state))
+
--- main.py
+++ main.py
@@ -1,63 +1,35 @@
-import json
-
-import paho.mqtt.client as mqtt
-
-subscribe_to = [
-    "zigbee2mqtt/Switch 1",
-    "zigbee2mqtt/Switch 2",
-    "zigbee2mqtt/Switch 3",
-    "zigbee2mqtt/Bulb 1",
-    "zigbee2mqtt/Bulb 2",
-]
+from api import Context, Switch, Bulb
 
 
-class Context:
-    def __init__(self, client, host):
-        self.client = client
-        self.host = host
-        self.last_known_states = {}
+class DefaultSwitch(Switch):
+    def __init__(self, context, name, bulb: Bulb):
+        super().__init__(context, name)
+        self.bulb = bulb
+        self._steps = 20
 
-    def on_connect(self, *_):
-        for subscription in subscribe_to:
-            self.client.subscribe(subscription)
+    def on_press(self, payload):
+        if self.bulb.is_on():
+            self.bulb.set_brightness(self.bulb.BRIGHTNESS_MAX)
+        else:
+            self.bulb.set_on()
 
-    def on_message(self, _, __, message):
-        payload = json.loads(message.payload.decode())
-        if "Switch" in message.topic:
-            self.default_switch_bulb_action(payload, message.topic.replace("Switch", "Bulb"))
+    def off_press(self, payload):
+        self.bulb.set_off()
 
-        self.last_known_states[message.topic] = payload
+    def down_press(self, payload):
+        self.bulb.decrease_brightness(self._steps)
 
-        print("topic: " + message.topic + ", payload: " + str(message.payload))
+    def up_press(self, payload):
+        self.bulb.increase_brightness(self._steps)
 
-    def loop(self):
-        self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
-        self.client.connect(self.host, 1883, 60)
-        self.client.loop_forever()
+context = Context("192.168.2.11")
 
-    def set_state(self, target, state):
-        self.client.publish(f'{target}/set', json.dumps(state))
+bulb1 = context.add_device(Bulb(context, 'zigbee2mqtt/Bulb 1'))
+bulb2 = context.add_device(Bulb(context, 'zigbee2mqtt/Bulb 2'))
+bulb3 = context.add_device(Bulb(context, 'zigbee2mqtt/Bulb 3'))
 
-    def default_switch_bulb_action(self, payload, target):
-        step = 10
-        last_known = self.last_known_states.get(target)
-        if last_known is None:
-            last_known = {'brightness': 125, 'state': 'OFF'}
-        if 'action' not in payload:
-            return
+switch1 = context.add_device(DefaultSwitch(context, 'zigbee2mqtt/Switch 1', bulb1))
+switch2 = context.add_device(DefaultSwitch(context, 'zigbee2mqtt/Switch 2', bulb2))
+switch3 = context.add_device(DefaultSwitch(context, 'zigbee2mqtt/Switch 3', bulb3))
 
-        if payload['action'] == 'on_press_release':
-            self.set_state(target, {"state": 'OFF' if last_known['state'] == 'ON' else 'ON'})
-        elif payload['action'] in ('up_press', 'up_hold'):
-            self.set_state(target, {"brightness": brightness(last_known['brightness'] + step)})
-        elif payload['action'] in ('down_press', 'down_hold'):
-            self.set_state(target, {"brightness": brightness(last_known['brightness'] - step)})
-
-
-
-def brightness(brightness):
-    return max(0, min(255, brightness))
-
-context = Context(mqtt.Client(mqtt.CallbackAPIVersion.VERSION2), "192.168.2.11")
-context.loop()
+context.start()
